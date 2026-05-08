93b14a9525735915844315027d7ed1da748e6670
Yorgos S <stefanakosgeorg@gmail.com>
2026-05-08 05:17:55 +0300

    progress

---
+++ my_plotter.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+movies = pd.read_csv("movies_from_2000.csv")
+
+# sns.scatterplot(data=movies, x="budget", y="revenue")
+sns.scatterplot(data=movies, x="runtime", y="revenue")
+# movies['release_date'] = pd.to_datetime(movies['release_date'], format='mixed')
+# sns.scatterplot(data=movies, x="release_date", y="revenue")
+# sns.scatterplot(data=movies, x="revenue", y="vote_average")
+# sns.scatterplot(data=movies, x="budget", y="vote_average")
+print(movies.corr(numeric_only=True))
+# print(movies[movies['budget'] == movies['budget'].max()]['title'])
+
+plt.show()
--- yearly_scraper.py
+++ yearly_scraper.py
@@ -22,12 +22,22 @@ movie.pop('origin_country')
 movie.pop('production_companies')
 movie.pop('production_countries')
 movie.pop('spoken_languages')
+
+releases = tmdb.Movies(25508).release_dates()['results']
+for release in releases:
+    if release['iso_3166_1'] == 'US':
+        movie['certification'] = 'NR'
+        for j in release['release_dates']:
+            if j['certification'] != '' and j['certification'] != 'NR':
+                movie['certification'] = j['certification']
+            
 df = pd.DataFrame(movie, index=[0])
 
 while i < total_pages:
     response = discover.movie(primary_release_year=years[0], page=i)
     total_pages = response["total_pages"]
-    print(f'Progress: {i/total_pages:.3f}% ({i:3d}/{total_pages})')
+    tmp_time = time.perf_counter()
+    print(f'Progress: {i*100/total_pages:.3f} %  ({i:3d}/{total_pages}) ({int((tmp_time - start)//60):2d} min elapsed)')
     results = response["results"]
 
     for result in results:
@@ -39,9 +49,18 @@ while i < total_pages:
         movie.pop('production_countries')
         movie.pop('spoken_languages')
 
+        releases = tmdb.Movies(result['id']).release_dates()['results']
+        for release in releases:
+            if release['iso_3166_1'] == 'US':
+                movie['certification'] = 'NR'
+                for j in release['release_dates']:
+                    if j['certification'] != '' and j['certification'] != 'NR':
+                        movie['certification'] = j['certification']
+
         df.loc[len(df)] = movie
     i += 1
     # if i == 3: break
+
 df = df.drop(0)
 df.to_csv(f'movies_from_{years[0]}.csv',index=False)
 
